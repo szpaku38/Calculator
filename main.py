03cd54d4e572dd69ea3029cb688b8b7ed2ff8e45
szpaku38 <szpaku38@gmail.com>
2022-01-07 19:06:01 +0100

    Repeating calculations.

--- main.py
+++ main.py
@@ -25,15 +25,28 @@ operations = {
     "/": divide
 }
 
+is_on = True
+
 num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
 
 for key in operations:
     print(key)
 
 operation_symbol = input("Pick an operation from the line above: ")
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
 
+while is_on:
+    num2 = int(input("What's the next number? "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    next_calc = input(
+        f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+
+    if next_calc == "n":
+        is_on = False
+    else:
+        num1 = answer
+        operation_symbol = input("Pick an operation: ")
