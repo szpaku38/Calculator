cbde9dbb2fdfa2cfb96e1a526d17bebf3ead7517
szpaku38 <szpaku38@gmail.com>
2022-01-07 19:15:36 +0100

    Finished.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from art import logo
-print(logo)
 
 
 def add(n1, n2):
@@ -25,28 +24,31 @@ operations = {
     "/": divide
 }
 
-is_on = True
 
-num1 = int(input("What's the first number? "))
+def calculator():
+    print(logo)
+    is_on = True
+    num1 = float(input("What's the first number? "))
 
-for key in operations:
-    print(key)
+    for key in operations:
+        print(key)
 
-operation_symbol = input("Pick an operation from the line above: ")
+    operation_symbol = input("Pick an operation from the line above: ")
 
-while is_on:
-    num2 = int(input("What's the next number? "))
+    while is_on:
+        num2 = float(input("What's the next number? "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+        next_calc = input(
+            f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation.: ")
+        if next_calc == "y":
+            num1 = answer
+            operation_symbol = input("Pick an operation: ")
+        else:
+            is_on = False
+            calculator()
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    next_calc = input(
-        f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
-
-    if next_calc == "n":
-        is_on = False
-    else:
-        num1 = answer
-        operation_symbol = input("Pick an operation: ")
+calculator()
